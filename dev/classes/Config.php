fd81192db1ac8a6af8de4e241a9a2dd394ab69bb
BastiaanAndriessen <bastiaan.andriessen@student.howest.be>
2013-07-08 11:51:38 +0200

    design mod + api setup

---
+++ dev/classes/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config
+{
+    const DB_TYPE = 'mysql';
+    const DB_HOST = 'localhost';
+    const DB_USER = 'osoc13';
+    const DB_PASS = 'bastiaan';
+    const DB_NAME = 'bastiaan';
+}
